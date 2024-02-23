64e0decb4413d46728b5b68bac395bcdc67b7927
Abishek Thamma <abishekthamma1@gmail.com>
2024-02-23 19:55:31 +0100

    custom Bpe in prepare and added torchrun command to testrunsh

--- nanoGPT/data/wikipedia_bpe/prepare.py
+++ nanoGPT/data/wikipedia_bpe/prepare.py
@@ -2,9 +2,22 @@ import os
 import requests
 import tiktoken
 import numpy as np
-
+from tokenizers import (
+    decoders,
+    models,
+    normalizers,
+    pre_tokenizers,
+    processors,
+    trainers,
+    Tokenizer)
+from transformers import PreTrainedTokenizerFast
+from tokenizers.normalizers import Lowercase, Strip, StripAccents, NFD
+from transformers import AutoTokenizer
 
 from pathlib import Path
+import pickle
+
+
 
 dataset_folder_path = Path(__file__).parents[3] / 'babylm_data'
 train_ds = 'babylm_10M'
@@ -14,27 +27,69 @@ dataset_name = "wikipedia"
 train_file_path = os.path.join(dataset_folder_path, train_ds, dataset_name + '.train')
 dev_file_path = os.path.join(dataset_folder_path, dev_ds, dataset_name + '.dev')
 
-train_file_path = os.path.join(dataset_folder_path, train_ds, dataset_name + '.train')
-dev_file_path = os.path.join(dataset_folder_path, dev_ds, dataset_name + '.dev')
+textfiles = [f"{dataset_folder_path}/{train_ds}/aochildes.train",
+             f"{dataset_folder_path}/{train_ds}/bnc_spoken.train",
+             f"{dataset_folder_path}/{train_ds}/cbt.train",
+             f"{dataset_folder_path}/{train_ds}/children_stories.train",
+             f"{dataset_folder_path}/{train_ds}/gutenberg.train",
+             f"{dataset_folder_path}/{train_ds}/open_subtitles.train",
+             f"{dataset_folder_path}/{train_ds}/qed.train",
+             f"{dataset_folder_path}/{train_ds}/simple_wikipedia.train",
+             f"{dataset_folder_path}/{train_ds}/switchboard.train",
+             f"{dataset_folder_path}/{train_ds}/wikipedia.train"]
 
 with open(train_file_path, 'r') as f:
     train_data = f.read()
-#print(f"length of train dataset in characters: {len(train_data):,}")
 
 with open(dev_file_path, 'r') as f:
     val_data = f.read()
-#print(f"length of val dataset in characters: {len(val_data):,}")
+
+
+
 
 meta = {}
 
-def encode_with_tokenizer(data, tokenizer = "customBPE", vocab_size = None):
+def encode_with_tokenizer(data, textfiles,  tokenizer_type = "customBPE", vocab_size = None, tokenizer_scratch = False, traintokenizer_1ds = False):
 
-    if tokenizer == "customBPE":
+    if tokenizer_type == "customBPE":
         # BUILD A CUSTOM BPE TOKENIZER
         meta["custom_tokenizer"] = True
-        pass
 
-    elif tokenizer == "gpt2":
+        #Check if tokenizer already exists
+
+        tokenizer_exists = True if os.path.exists("tokenizer.json") else False
+
+        if tokenizer_scratch or not tokenizer_exists:
+
+            tokenizer = Tokenizer(models.BPE())
+            tokenizer.normalizer = normalizers.Sequence([NFD(), Lowercase(), Strip(), StripAccents()])
+            tokenizer.pre_tokenizer = pre_tokenizers.ByteLevel(add_prefix_space=False)
+            trainer = trainers.BpeTrainer(vocab_size=vocab_size, special_tokens=["<|endoftext|>"])
+
+            if traintokenizer_1ds:
+                textfiles = [train_file_path]
+
+            tokenizer.train(files=textfiles, trainer=trainer)
+
+            tokenizer.post_processor = processors.ByteLevel(trim_offsets=True)
+
+            # Save the tokenizer
+            wrapped_tokenizer = PreTrainedTokenizerFast(
+                tokenizer_object=tokenizer,
+                bos_token="<|endoftext|>",
+                eos_token="<|endoftext|>")
+
+            print("Saving tokenizer to", Path(__file__).parents[0])
+            wrapped_tokenizer.save_pretrained(Path(__file__).parents[0])
+            tokenizer = AutoTokenizer.from_pretrained(Path(__file__).parents[0])
+
+        else:
+            tokenizer = AutoTokenizer.from_pretrained(Path(__file__).parents[0])
+
+        data_ids = tokenizer.encode(data)
+        meta["vocab_size"] = tokenizer.vocab_size
+
+    elif tokenizer_type == "gpt2":
         meta["custom_tokenizer"] = False
         enc = tiktoken.get_encoding("gpt2")
         data_ids = enc.encode_ordinary(data)
@@ -43,8 +98,8 @@ def encode_with_tokenizer(data, tokenizer = "customBPE", vocab_size = None):
 
 
 # encode with tiktoken gpt2 bpe
-train_ids = encode_with_tokenizer(train_data, tokenizer = "gpt2")
-val_ids = encode_with_tokenizer(val_data, tokenizer =  "gpt2")
+train_ids = encode_with_tokenizer(train_data, textfiles, tokenizer_type = "customBPE", vocab_size = 16000)
+val_ids = encode_with_tokenizer(val_data, textfiles, tokenizer_type =  "customBPE", vocab_size = 16000)
 
 print(f"train has {len(train_ids):,} tokens")
 print(f"val has {len(val_ids):,} tokens")
@@ -57,5 +112,15 @@ val_ids = np.array(val_ids, dtype=np.uint16)
 train_ids.tofile(os.path.join(os.path.dirname(__file__), 'train.bin'))
 val_ids.tofile(os.path.join(os.path.dirname(__file__), 'val.bin'))
 
+with open(os.path.join(os.path.dirname(__file__), 'meta.pkl'), 'wb') as f:
+    pickle.dump(meta, f)
+
+# When using GPT tokenizer:
 # train has 1,377,181 tokens
 # val has 1,594,771 tokens
+
+
+# When using custom BPE tokenizer:
+
+# train has 1,426,302 tokens
+# val has 1,667,685 tokens
--- test_run_job.sh
+++ test_run_job.sh
@@ -7,15 +7,35 @@
 #SBATCH --gpus-per-node=4
 #SBATCH --output=logs/%j.out
 #SBATCH --error=logs/%j.err
-#SBATCH -t 1:30:00
+#SBATCH -t 2:30:00
 
 echo $SLURM_JOB_ID
 
 module load 2023
 module load Python/3.11.3-GCCcore-12.3.0
 
-source $HOME/repo/ss-llm/mt1/bin/activate
+source $HOME/repo/mt1/bin/activate
 
 cd $HOME/repo/ss-llm/nanoGPT
 
-python train.py config/simple_wikipedia_char.py
+#python train.py config/train_simplewiki_char.py
+
+torchrun --standalone --nproc_per_node=4 train.py config/train_wikipedia_gpt.py
+
+
+#This training script can be run both on a single gpu in debug mode,
+#and also in a larger training run with distributed data parallel (ddp).
+#
+#To run on a single GPU, example:
+#$ python train.py --batch_size=32 --compile=False
+#
+#To run with DDP on 4 gpus on 1 node, example:
+#$ torchrun --standalone --nproc_per_node=4 train.py
+#
+#To run with DDP on 4 gpus across 2 nodes, example:
+#- Run on the first (master) node with example IP 123.456.123.456:
+#$ torchrun --nproc_per_node=8 --nnodes=2 --node_rank=0 --master_addr=123.456.123.456 --master_port=1234 train.py
+#- Run on the worker node:
+#$ torchrun --nproc_per_node=8 --nnodes=2 --node_rank=1 --master_addr=123.456.123.456 --master_port=1234 train.py
+#(If your cluster does not have Infiniband interconnect prepend NCCL_IB_DISABLE=1)
+#"""
